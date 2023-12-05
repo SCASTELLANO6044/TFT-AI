d0d6c90a1bce168d9a8c2230fa6984a0ab903455
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-12-05 13:30:53 +0000

    :construction: separate adding predict button

--- AI/view/view.py
+++ AI/view/view.py
@@ -2,28 +2,23 @@ import customtkinter
 from controller.controller import Controller
 
 
+class OutputFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+
 def predict_button_action():
     Controller.run_ai()
 
 
-class InputFrame(customtkinter.CTkFrame):
-    def __int__(self, master):
+class IntputFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
         super().__init__(master)
-        self.grid_columnconfigure(0, 1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(2, weight=1)
 
-        checkbox = customtkinter.CTkCheckBox(self, text="test")
-        checkbox.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
-        predict_button = customtkinter.CTkButton(self, text="Predict", command=predict_button_action)
-        predict_button.grid(row=1, column=1, padx=20, pady=20, sticky="ew")
-
-
-class OutputFrame(customtkinter.CTkFrame):
-    def __int__(self, master):
-        super().__init__(master)
-        self.grid_columnconfigure(0, 1)
-
-        checkbox = customtkinter.CTkCheckBox(self, text="test")
-        checkbox.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.button = customtkinter.CTkButton(self, text="Predict", command=predict_button_action)
+        self.button.grid(row=2, column=0, padx=10, pady=10, sticky="sew")
 
 
 class App(customtkinter.CTk):
@@ -32,14 +27,16 @@ class App(customtkinter.CTk):
 
         self.title("TFT-AI")
         self.geometry("1000x500")
-        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.input_frame = InputFrame(self)
-        self.input_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
-        self.input_frame.configure(fg_color="transparent")
         self.output_frame = OutputFrame(self)
-        self.output_frame.grid(row=0, column=1, padx=(0, 10), pady=(10, 0), sticky="nsew")
+        self.output_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+        self.output_frame.configure(fg_color="transparent")
+
+        self.input_frame = IntputFrame(self)
+        self.input_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
 
 
 app = App()
