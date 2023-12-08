e241bf64b63c8268ad688b8cd5a650eac0477e08
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-12-08 23:14:21 +0000

    :zap: model 5xfaster

--- AI/model/model.py
+++ AI/model/model.py
@@ -70,34 +70,19 @@ def get_model():
     """
     model = tf.keras.models.Sequential([
 
-        tf.keras.layers.Conv2D(
-            32,
-            (3, 3),
-            activation="relu",
-            input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)
-        ),
+        tf.keras.layers.Conv2D(16, (3, 3), 1, activation="relu", input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+        tf.keras.layers.MaxPooling2D(),
 
-        tf.keras.layers.MaxPooling2D(
-            pool_size=(2, 2)
-        ),
+        tf.keras.layers.Conv2D(32, (3, 3), 1, activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+
+        tf.keras.layers.Conv2D(16, (3, 3), 1, activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
 
         tf.keras.layers.Flatten(),
 
         tf.keras.layers.Dense(256, activation="relu"),
-        tf.keras.layers.Dense(256, activation="relu"),
-        tf.keras.layers.Dense(256, activation="relu"),
-        tf.keras.layers.Dense(256, activation="relu"),
-        tf.keras.layers.Dense(256, activation="relu"),
-        tf.keras.layers.Dense(256, activation="relu"),
-        tf.keras.layers.Dense(256, activation="relu"),
-        tf.keras.layers.Dense(256, activation="relu"),
-        tf.keras.layers.Dense(256, activation="relu"),
-
-        tf.keras.layers.Dropout(0.05),
-
-        tf.keras.layers.Dense(
-            NUM_CATEGORIES,
-            activation="softmax")
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
 
     model.compile(
