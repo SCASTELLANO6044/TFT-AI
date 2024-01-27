bd0514c62c9dfdee72a55f14dbc922749cf03164
SCASTELLANO6044 <sergiocastellanotrabajo@gmail.com>
2024-01-27 19:48:56 +0100

    :zap: Improve style

--- AI/view/input_view.py
+++ AI/view/input_view.py
@@ -13,12 +13,12 @@ class IntputFrame(customtkinter.CTkFrame):
                                                     offvalue="off")
         self.theme_switch.grid(row=0, column=0, padx=0, pady=10, sticky="ne")
 
-        self.file_entry = customtkinter.CTkButton(self, text="Busca tu imagen", command=master.browse_file,
+        self.file_entry = customtkinter.CTkButton(self, text="Busca tu imagen.", command=master.browse_file,
                                                   fg_color="#F39A0E", hover_color="#c47a0b", font=("Helvetica", 15),
                                                   text_color="#33383F")
         self.file_entry.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 
-        self.predict_button = customtkinter.CTkButton(self, text="Calcular predicción", command=master.predict_button_action,
+        self.predict_button = customtkinter.CTkButton(self, text="Calcular predicción.", command=master.predict_button_action,
                                                       fg_color="#0E68A4", hover_color="#0b4c77", font=("Helvetica", 15),
                                                       text_color="#33383F")
         self.predict_button.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
--- AI/view/output_view.py
+++ AI/view/output_view.py
@@ -18,7 +18,7 @@ class OutputFrame(customtkinter.CTkFrame):
         self.label = customtkinter.CTkLabel(self, image=self.my_image, text="")
         self.label.grid(row=1, column=0, padx=0, pady=0, sticky="ew")
 
-        self.image_label = customtkinter.CTkLabel(self, text= "", font=("Helvetica", 23))
+        self.image_label = ImageLabelFrame(self)
         self.image_label.grid(row=2, column=0, padx=0, pady=(0,50), sticky="nsew")
 
     def display_image(self, image_path):
@@ -30,4 +30,15 @@ class OutputFrame(customtkinter.CTkFrame):
         return self.image_path
 
     def set_image_label(self, prediction):
-        self.image_label.configure(text="Predicción: "+prediction)
+        self.image_label.image_label.configure(text="Predicción: "+prediction)
+
+
+class ImageLabelFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+
+        self.image_label = customtkinter.CTkLabel(self, text="Predicción: ", font=("Helvetica", 23))
+        self.image_label.grid(row=0, column=0, padx=0, pady=8, sticky="nsew")
