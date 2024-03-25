9e1b27c6eb200d52e1690b89ec9b97a3c893d09e
SCASTELLANO6044 <sergiocastellanotrabajo@gmail.com>
2024-03-25 23:15:04 +0100

    :zap: delete log file and create it again before every execution

--- AI/model/model.py
+++ AI/model/model.py
@@ -3,16 +3,11 @@ import numpy as np
 import os
 import tensorflow as tf
 import logging
+import sys
 from model.utils import Utils
 from sklearn.model_selection import train_test_split
 
-logging.basicConfig(format="%(asctime)s [%(levelname)s] - [%(filename)s > %(funcName)s() > %(lineno)s] - %(message)s",
-                    datefmt="%H:%M:%S",
-                    filename=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', 'log.log'))
-
-logging.getLogger().setLevel(logging.INFO)
-
-EPOCHS = 50
+EPOCHS = 3
 IMG_WIDTH = 100
 IMG_HEIGHT = 75
 MODEL_NAME = "my_model.keras"
@@ -23,6 +18,16 @@ IMAGES_FOLDER_PART1 = os.path.join(os.path.dirname(os.path.abspath(__file__)), '
                                    'HAM10000_images_part_1')
 IMAGES_FOLDER_PART2 = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', 'dataverse_files',
                                    'HAM10000_images_part_2')
+LOG_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', 'log.log')
+
+if os.path.exists(LOG_FILE_PATH):
+    os.remove(LOG_FILE_PATH)
+
+logging.basicConfig(format="%(asctime)s [%(levelname)s] - [%(filename)s > %(funcName)s() > %(lineno)s] - %(message)s",
+                    datefmt="%H:%M:%S",
+                    filename=LOG_FILE_PATH)
+
+logging.getLogger().setLevel(logging.INFO)
 
 CATEGORIES_DICT = {0: 'Enfermedad de Bowen.',
                    1: 'Carcinoma de células basales.',
@@ -76,61 +81,63 @@ class Model:
 
     @staticmethod
     def main(image_path):
+        with open(LOG_FILE_PATH, 'w') as log_file:
+            sys.stdout = log_file
+            try:
+                logging.info(msg="Starting to load model from file.")
 
-        try:
-            logging.info(msg="Starting to load model from file.")
+                model = tf.keras.models.load_model(os.path.join(MODEL_DIR, MODEL_NAME))
 
-            model = tf.keras.models.load_model(os.path.join(MODEL_DIR, MODEL_NAME))
+                logging.info(msg="Finished to load model from file.")
 
-            logging.info(msg="Finished to load model from file.")
+            except OSError:
+                logging.info(msg="Model not found in files.")
 
-        except OSError:
-            logging.info(msg="Model not found in files.")
+                logging.info(msg="Starting to load data.")
 
-            logging.info(msg="Starting to load data.")
+                labels, images = load_data()
 
-            labels, images = load_data()
+                x_train, x_test, y_train, y_test = train_test_split(
+                    images, labels, test_size=0.2, random_state=42
+                )
 
-            x_train, x_test, y_train, y_test = train_test_split(
-                images, labels, test_size=0.2, random_state=42
-            )
+                logging.info("Finished to load data.")
 
-            logging.info("Finished to load data.")
+                logging.info("Starting to configure model.")
 
-            logging.info("Starting to configure model.")
+                model = get_model()
 
-            model = get_model()
+                early_stopping = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience=5,
+                                                                  restore_best_weights=True)
 
-            early_stopping = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience=5, restore_best_weights=True)
+                logging.info("Finished to configure model.")
 
-            logging.info("Finished to configure model.")
+                logging.info("Starting to train and evaluate model.")
 
-            logging.info("Starting to train and evaluate model.")
+                model.fit(x_train, y_train, epochs=EPOCHS, callbacks=[early_stopping], verbose=2)
 
-            model.fit(x_train, y_train, epochs=EPOCHS, callbacks=[early_stopping], verbose=1)
+                model.evaluate(x_test, y_test, verbose=2)
 
-            model.evaluate(x_test, y_test, verbose=2)
+                logging.info("Finished to train and evaluate model.")
 
-            logging.info("Finished to train and evaluate model.")
+                logging.info("Starting to save the model.")
 
-            logging.info("Starting to save the model.")
+                model.save(filepath=os.path.join(MODEL_DIR, MODEL_NAME), overwrite=True, save_format="keras")
 
-            model.save(filepath=os.path.join(MODEL_DIR, MODEL_NAME), overwrite=True, save_format="keras")
+                logging.info("Finished to save the model.")
 
-            logging.info("Finished to save the model.")
+            logging.info("Starting to prepare the prediction.")
 
-        logging.info("Starting to prepare the prediction.")
+            img_to_predict = cv2.imread(image_path)  # Replace with the path to your test image
+            img_to_predict = cv2.resize(img_to_predict, (IMG_WIDTH, IMG_HEIGHT))
+            img_to_predict = np.expand_dims(img_to_predict, axis=0)
 
-        img_to_predict = cv2.imread(image_path)  # Replace with the path to your test image
-        img_to_predict = cv2.resize(img_to_predict, (IMG_WIDTH, IMG_HEIGHT))
-        img_to_predict = np.expand_dims(img_to_predict, axis=0)
+            # Make a prediction
+            prediction = model.predict(img_to_predict)
 
-        # Make a prediction
-        prediction = model.predict(img_to_predict)
+            # Get the category with the highest probability
+            predicted_category = np.argmax(prediction)
 
-        # Get the category with the highest probability
-        predicted_category = np.argmax(prediction)
+            logging.info("Finished to prepare the prediction.")
 
-        logging.info("Finished to prepare the prediction.")
-
-        return CATEGORIES_DICT.get(predicted_category)
+            return CATEGORIES_DICT.get(predicted_category)
